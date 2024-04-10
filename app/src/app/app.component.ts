ad020ec4326b3499a925e45c200e72e22bee79bd
Geetansh Yadav <geetansh.yadhav@windstream.com>
2024-04-10 22:48:40 +0530

    mobile responsive test apr 10

--- app/src/app/app.component.ts
+++ app/src/app/app.component.ts
@@ -3,14 +3,20 @@ import { RouterOutlet } from '@angular/router';
 import { CarouselComponent } from './carousel/carousel.component';
 import { FeaturesComponent } from './features/features.component';
 import { RecentProjectsComponent } from './recent-projects/recent-projects.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,CarouselComponent,FeaturesComponent,RecentProjectsComponent],
+  imports: [RouterOutlet,CarouselComponent,FeaturesComponent,RecentProjectsComponent,CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'app';
+  openMenu:boolean=true;
+  onClickButton(){
+    console.log('hii');
+    
+  }
 }
--- app/tailwind.config.js
+++ app/tailwind.config.js
@@ -4,7 +4,11 @@ module.exports = {
     "./src/**/*.{html,ts}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontSize:{
+        '10':'10px'
+      }
+    },
   },
   plugins: [],
 }
