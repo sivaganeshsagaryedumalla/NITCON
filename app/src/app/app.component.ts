ace53424fe6d60d59df42f5cc65e9b7af8e06cb4
sivaganeshsagaryedumalla <143327329+sivaganeshsagaryedumalla@users.noreply.github.com>
2024-04-14 19:48:46 +0530

    added footer

--- app/src/app/app.component.ts
+++ app/src/app/app.component.ts
@@ -4,19 +4,20 @@ import { CarouselComponent } from './carousel/carousel.component';
 import { FeaturesComponent } from './features/features.component';
 import { RecentProjectsComponent } from './recent-projects/recent-projects.component';
 import { CommonModule } from '@angular/common';
+import { FooterComponent } from './footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,CarouselComponent,FeaturesComponent,RecentProjectsComponent,CommonModule],
+  imports: [RouterOutlet, CarouselComponent, FeaturesComponent, RecentProjectsComponent, CommonModule, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'app';
-  openMenu:boolean=true;
-  onClickButton(){
+  openMenu: boolean = true;
+  onClickButton() {
     console.log('hii');
-    
+
   }
 }
