19887bac553482e645d7bb701ee1d6cde2814da8
sivaganeshsagaryedumalla <143327329+sivaganeshsagaryedumalla@users.noreply.github.com>
2024-04-21 22:36:42 +0530

    Adding UI changes

--- app/src/app/features/features.component.ts
+++ app/src/app/features/features.component.ts
@@ -11,47 +11,60 @@ import { Component, ElementRef, NgZone, Renderer2 } from '@angular/core';
 })
 export class FeaturesComponent {
   services = [
-    { title: 'Home Construction', desc: 'Expert construction services for building your dream home.', path: 'assets/images/service-icon1.png' },
-    { title: 'Building Remodels', desc: 'Transform your space with professional building renovation solutions.', path: 'assets/images/service-icon2.png' },
-    { title: 'Interior Design', desc: 'Elevate your interior spaces with personalized design expertise.', path: 'assets/images/service-icon3.png' },
+    { title: 'Home Construction', desc: 'Expert construction services for building your dream home.', path: 'assets/images/service-icon1.png', class: 'cls1' },
+    { title: 'Building Remodels', desc: 'Transform your space with professional building renovation solutions.', path: 'assets/images/service-icon2.png', class: 'cls2' },
+    { title: 'Interior Design', desc: 'Elevate your interior spaces with personalized design expertise.', path: 'assets/images/service-icon3.png', class: 'cls3' },
     // { title: null, desc: 'Comprehensive service center offering various solutions for your needs.', path: 'assets/images/service-center.jpg' },
-    { title: 'Exterior Design', desc: 'Enhance your property\'s curb appeal with innovative exterior design solutions.', path: 'assets/images/service-icon4.png' },
-    { title: 'Renovation', desc: 'Revitalize your property with top-notch renovation services tailored to your needs.', path: 'assets/images/service-icon5.png' },
-    { title: 'Safety Management', desc: 'Prioritize safety with expert management solutions for peace of mind.', path: 'assets/images/service-icon6.png' }
+    { title: 'Exterior Design', desc: 'Enhance your property\'s curb appeal with innovative exterior design solutions.', path: 'assets/images/service-icon4.png', class: 'cls4' },
+    { title: 'Renovation', desc: 'Revitalize your property with top-notch renovation services tailored to your needs.', path: 'assets/images/service-icon5.png', class: 'cls5' },
+    { title: 'Safety Management', desc: 'Prioritize safety with expert management solutions for peace of mind.', path: 'assets/images/service-icon6.png', class: 'cls6' }
   ];
+  className: any;
+  activeIndex: any;
 
 
   constructor(private renderer: Renderer2,
     private elementRef: ElementRef,
-    private ngZone: NgZone){
-      
-    }
+    private ngZone: NgZone) {
 
-    // ngOnInit(){
-    //   this.observeScroll();
-    // }
-    // observeScroll(): void {
-    //   const target=this.elementRef.nativeElement.querySelector('.box');
-    //   const options = {
-    //     threshold: 0.5 // Adjust the threshold as needed
-    //   };
-    //   const observer=new IntersectionObserver((entries)=>{
-    //     entries.forEach((entry:any)=>{
-    //       if(entry.isIntersecting){
-    //         this.ngZone.run(()=>{
-    //           this.renderer.addClass(this.elementRef.nativeElement.querySelector('.box'),'scrolled')
-    //         })
-    //       }
-    //       else{
-    //         this.ngZone.run(()=>[
-    //           this.renderer.addClass(this.elementRef.nativeElement.querySelector('.box'),'scrolled')
-    //         ])
-    //       }
+  }
 
-    //     })
-    //   },options)
-    //   observer.observe(target);
-    // }
-    
-  
+  onTouchStart(e: any, index: number) {
+    this.className = e.class;
+    this.activeIndex = index;
+
+  }
+  onMouseEnter(e: any, index: number) {
+    this.className = e.class;
+    this.activeIndex = index;
+
+  }
+
+  // ngOnInit(){
+  //   this.observeScroll();
+  // }
+  // observeScroll(): void {
+  //   const target=this.elementRef.nativeElement.querySelector('.box');
+  //   const options = {
+  //     threshold: 0.5 // Adjust the threshold as needed
+  //   };
+  //   const observer=new IntersectionObserver((entries)=>{
+  //     entries.forEach((entry:any)=>{
+  //       if(entry.isIntersecting){
+  //         this.ngZone.run(()=>{
+  //           this.renderer.addClass(this.elementRef.nativeElement.querySelector('.box'),'scrolled')
+  //         })
+  //       }
+  //       else{
+  //         this.ngZone.run(()=>[
+  //           this.renderer.addClass(this.elementRef.nativeElement.querySelector('.box'),'scrolled')
+  //         ])
+  //       }
+
+  //     })
+  //   },options)
+  //   observer.observe(target);
+  // }
+
+
 }
